171401293ea8d2ff541d4770cc0162fe53c24024
Krushil <krushilnaik96@gmail.com>
2021-04-12 09:42:01 -0400

    ditch manifest file and set stage for redux

---
+++ src/controllers/store/store.js
@@ -0,0 +1,6 @@
+/**
+ * Since we have multiple components trying to work with each other
+ * 	(Reddit, ApexCharts, Movers)
+ * We'll need to use Redux to store information that both will need
+ * instead of just storing everything in each component's state
+ */
