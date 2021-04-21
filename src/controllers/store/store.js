29f491a79208e8723ff2d0cd219b29bc1a32d0f2
Krushil <krushilnaik96@gmail.com>
2021-04-21 13:51:21 -0400

    Drop Redux with plans to use Context API

--- src/controllers/store/store.js
+++
@@ -1,6 +0,0 @@
-/**
- * Since we have multiple components trying to work with each other
- * 	(Reddit, ApexCharts, Movers)
- * We'll need to use Redux to store information that both will need
- * instead of just storing everything in each component's state
- */
