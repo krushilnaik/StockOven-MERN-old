3e929363017788879c77613790fe8d92484f9780
Krushil <krushilnaik96@gmail.com>
2021-04-21 10:53:53 -0400

    Begin integrating toggles into UI

--- src/views/components/GraphComponent.jsx
+++ src/views/components/GraphComponent.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import '../scss/GraphComponent.scss';
+import ToggleGroup from './ToggleGroup';
 
 function GraphComponent() {
 	return (
 		<section id='graph'>
 			<div className='apex-chart'>PLACEHOLDER GRAPH</div>
-			<div className='toggles'>PLACEHOLDER TOGGLES</div>
+			<div className='toggles'>
+				<ToggleGroup id='ranges' values={['1d', '5d', '1mo']} />
+				<ToggleGroup id='intervals' values={['5m', '15m', '60m', '1d']} />
+			</div>
 		</section>
 	);
 }
--- src/views/components/ToggleGroup.jsx
+++ src/views/components/ToggleGroup.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
-function ToggleGroup() {
-	return <div>TOGGLE GROUP</div>;
+import '../scss/ToggleGroup.scss';
+
+/**
+ *
+ * @param {{
+ * 	id: string
+ * 	values: string[]
+ * }} props
+ * @returns
+ */
+function ToggleGroup(props) {
+	return (
+		<>
+			<div id={props.id} style={{ display: 'none' }}>
+				{props.values.map(value => (
+					<input
+						key={`${props.id}-${value}-label`}
+						type='radio'
+						name={props.id}
+						id={`${props.id}-${value}-label`}
+					/>
+				))}
+			</div>
+
+			<div id={`${props.id}-labels`}>
+				<div id={`${props.id}-ui`}></div>
+				<div className='modes'>
+					{props.values.map(value => (
+						<label key={`${props.id}-${value}-label`} htmlFor={`${props.id}-${value}-label`}>
+							{value}
+						</label>
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default ToggleGroup;
