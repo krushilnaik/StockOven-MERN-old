a9160f720122f5ad13d95fccd48435af347dcb9a
Krushil <krushilnaik96@gmail.com>
2021-04-21 20:33:30 -0400

    Use oven knobs as view switches (log in, new user)

--- src/views/components/SignInForm.jsx
+++ src/views/components/SignInForm.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import { faPlus, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 
 import '../scss/SignInForm.scss';
 
 function SignInForm() {
+	const [page, setPage] = useState('Sign in');
+
+	/**
+	 * @param {React.SyntheticEvent<HTMLButtonElement>} event
+	 */
+	const logIn = event => {
+		event.preventDefault();
+
+		// attempt to log the user in
+	};
+
 	return (
 		<div className='sign-in'>
-			{/* This is just for show. */}
 			<div className='controls'>
-				<div className='dial'></div>
-				<div className='dial'></div>
+				<div
+					className='dial'
+					onClick={() => {
+						setPage('Create user');
+					}}
+				>
+					<FontAwesomeIcon icon={faSignInAlt} />
+				</div>
+				<div
+					className='dial'
+					onClick={() => {
+						setPage('Create user');
+					}}
+				>
+					<FontAwesomeIcon icon={faPlus} />
+				</div>
 			</div>
 
-			{/* The actual form */}
-			<form></form>
+			<div className='form-wrapper'>
+				<form>
+					<h1>{page}</h1>
+					<input type='text' name='username' id='username' placeholder='Username:' />
+					<input type='password' name='password' id='password' placeholder='Password:' />
+					<button type='submit' onClick={logIn}>
+						Log In
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 }
