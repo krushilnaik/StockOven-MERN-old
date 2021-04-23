a1fb3fe2f964e5da5c4da07a1a2d43f3a910b9a2
Krushil <krushilnaik96@gmail.com>
2021-04-23 08:11:30 -0400

    Consolidate /auth routes into single file

--- src/controllers/routes/api.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-// router.get
-
-// router.post
-
-// router.put
-
-module.exports = router;
--- src/controllers/routes/auth.routes.js
+++ src/controllers/routes/auth.routes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
+const passport = require('passport');
 
-const googleRoutes = require('./auth/google.routes');
-const yahooRoutes = require('./auth/yahoo.routes.js');
-const redditRoutes = require('./auth/reddit.routes');
+// google auth
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
-router.get('/google', googleRoutes);
-router.get('/yahoo', yahooRoutes);
-router.get('/reddit', redditRoutes);
+router.get('/google/redirect', passport.authenticate('google', { failureRedirect: '/sign_in' }), (_req, res) => {
+	res.send('Successfully logged in with Google');
+});
+
+// reddit auth
+router.get('/reddit', passport.authenticate('reddit'));
+
+router.get('/reddit/redirect', passport.authenticate('reddit', { failureRedirect: '/sign_in' }), (_req, res) => {
+	res.send('Successfully logged in with Reddit');
+});
 
 module.exports = router;
